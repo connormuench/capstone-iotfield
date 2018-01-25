b43fff0c835fd598b342fa83cae431d9c96f118f
Connor Muench <csmuench@ucalgary.ca>
2018-01-25 16:22:50 -0700

    Fix check_admin method.

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # Check if the current user is an administrator
   def check_admin(url)
     if !current_user.is_admin
-      redirect_to(url), alert: 'You need administrator privileges to perform that task.'
+      redirect_to(url, alert: 'You need administrator privileges to perform that task.')
     end
   end
 end
