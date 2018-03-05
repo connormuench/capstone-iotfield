6bb3b34704a98a14a6a0bc4afba5eddfb44d0dfd
Reid Sigsworth <wisp4400@gmail.com>
2018-03-04 21:51:49 -0700

    Admin and user views, doesn't change database yet

--- webapp/app/controllers/users_controller.rb
+++ webapp/app/controllers/users_controller.rb
@@ -1,20 +1,34 @@
 class UsersController < ApplicationController
 
 
+   before_action :set_user, only: [:show, :update, :destroy]
+
    # GET /facilities/1/sensors/1
   def show
-    @user = User.find(params[:id])
+    
   end
 
 
   def create
     @user = User.new
+    @user.id=params[:id]
+    @user.username=params[:username]
+    @user.email=params[:email]
+    @user.is_admin=params[:is_admin]
+    @user.name=params[:name]
+    @user.save
+
   end
 
 
   # PATCH/PUT /facilities/1/sensors/1
   def update
-
+    @user.id=params[:id]
+    @user.username=params[:username]
+    @user.email=params[:email]
+    @user.is_admin=params[:is_admin]
+    @user.name=params[:name]
+    @user.save
   end
 
 
@@ -23,11 +37,14 @@ class UsersController < ApplicationController
 
   def destroy
     #get rid of permissions
+
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def point_params
       params.require(:point).permit(:name, :description, :location)
